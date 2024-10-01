1ca018d119f037500dc0fc4c2a399aee986a9dba
Aquil Hassan <134952818+Aquil-Hassan@users.noreply.github.com>
2024-10-01 18:32:49 +0000

    conversion_learning

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,24 @@
+let score=33
+
+console.log(typeof (score))
+let value=String(score)
+console.log(typeof(value))
+
+
+// "33"==33
+// "33aba"=>NaN not a number
+// true=>1 false=>0
+
+
+// number to bool 
+// any number is converted to true
+// 0 is converted to false;
+
+let a=100;
+let tocheck=Boolean(a);
+console.log(tocheck)
+
+
+// similarly empty string is false;
+// if length of string is greater than 0 then true;
+
