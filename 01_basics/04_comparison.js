2b956c89ff97a2b800ed400342b28415def12bac
Aquil Hassan <134952818+Aquil-Hassan@users.noreply.github.com>
2024-10-01 19:10:08 +0000

    datatypesummary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("02">1);
+console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);  //not a proper comparison
+
+
+
+// == do conversion and check
+// === datatype and value both check
+
+
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// primitive - call by value
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// datatype is not defined in variable name: therefore javascript is dynamically typed language
+
+
+// if value is not given then undefined value is given to it automatically
+
+const id=Symbol('123'); //symbol same value pr bhi different hota h
+const anotherId=Symbol('123');
+console.log(id==anotherId);
+
+
+const bigNumber=34453245624352354n //n at last convert it into big int
+
+
+
+
+// Reference type (Non primitive)
+// Arrays, Objects, Functions
+
+
+const heros=["shaktiman","naagraj", "doga"];
+
+
+let myObj={
+    name:"aquil",age:"22",
+}
+
+const myFunction=function(){
+    console.log("hello world");
+}
+
+console.log( typeof myFunction);
