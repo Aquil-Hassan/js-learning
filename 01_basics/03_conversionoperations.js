9205b7e2f5abbcf27721e0f85be3f43541098f6c
Aquil Hassan <134952818+Aquil-Hassan@users.noreply.github.com>
2024-10-01 18:47:39 +0000

    conversion_continued

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -22,3 +22,31 @@ console.log(tocheck)
 // similarly empty string is false;
 // if length of string is greater than 0 then true;
 
+let anumber=3;
+let negnumber=-anumber
+console.log(negnumber)
+
+// basic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log((2**3));
+// console.log(2/3);
+// console.log(2%5);
+
+
+let str1="aquil";
+let str2="hassan";
+let str3=str1+str2;
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(2+"1"+3);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(+true);possible
+// console.log(true+)  //notpossible
